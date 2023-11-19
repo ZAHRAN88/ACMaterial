1d89581d87d9c8dc0db9f38ec4e08584fa197089
ZAHRAN88 <124225355+ZAHRAN88@users.noreply.github.com>
2023-11-19 15:25:24 +0200

    OOP resources

---
+++ courses/modal.js
@@ -0,0 +1,8 @@
+function showDelayedModal() {
+    $('#myModal').modal('show');
+  }
+  
+  // Show the modal after 10 seconds of page load
+  $(document).ready(function() {
+    setTimeout(showDelayedModal, 1000); // 10000 milliseconds = 10 seconds
+  });
