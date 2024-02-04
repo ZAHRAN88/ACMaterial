bd9f0895222450dfaeff62704a2eae4368eb208a
ZAHRAN88 <124225355+ZAHRAN88@users.noreply.github.com>
2024-02-04 11:15:04 +0200

    Chatbot integration

---
+++ assets/Scripts/bot.js
@@ -0,0 +1,11 @@
+<script>
+window.embeddedChatbotConfig = {
+chatbotId: "PLnWdVNB0QVlPLtIdgPGq",
+domain: "www.chatbase.co"
+}
+</script>
+
+src="https://www.chatbase.co/embed.min.js"
+chatbotId="PLnWdVNB0QVlPLtIdgPGq"
+domain="www.chatbase.co"
+defer>
